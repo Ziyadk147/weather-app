1e2fc1b34234a86b616f3fa9d47367c5d5d7858d
Muhammad Ziyad Khan <ziyadkhan2333@gmail.com>
2023-09-06 19:12:16 +0500

    integrated the weather api

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class WeatherController extends Controller
+{
+    public function getWeather(Request $request)
+    {
+        $apikey = '761d0269fe0145caa47201101232008';
+
+        $city = $request->city;
+
+        $client = new Client();
+
+        $response = $client->get('http://api.weatherapi.com/v1/current.json?key='.$apikey.'&q='.$city);
+
+        $data = json_decode($response->getBody());
+
+        dd($data);
+
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('main.main');
 });
+Route::get('/ss' ,[ \App\Http\Controllers\WeatherController::class,'getWeather']);
